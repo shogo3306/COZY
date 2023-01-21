2ed9bca32689f42741b467cbb52a2c206da7a3d1
shogo3306 <shogosoccer6@icloud.com>
2023-01-21 19:32:23 +0900

    ソースコードup

---
+++ js/script.js
@@ -0,0 +1,69 @@
+function fadeAnime() {
+
+  $('.fadeUpTrigger').each(function () { //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top - 20;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    } else {
+      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+//about.htmlのスライダー
+$('.slider').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 6900,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 3,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+      breakpoint: 769,//モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 2,//スライドを画面に2枚見せる
+      }
+    },
+    {
+      breakpoint: 426,//モニターの横幅が426px以下の見せ方
+      settings: {
+        slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+      }
+    }
+  ]
+});
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+  $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+jQuery(window).on('scroll', function () {
+  if (400 < jQuery(this).scrollTop()) {
+    jQuery('.logo').addClass('change-color');
+  } else {
+    jQuery('.logo').removeClass('change-color');
+  }
+});
